23b85ff849d35c2b0762d06fc5e0438839058476
jeanfredrik <jeanfredrik@gmail.com>
2015-08-12 13:29:10 +0200

    bump and publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jeanfredrik:reactive-sort-specifier',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Create sort specifiers for your collection.find calls that are reactive and easy to manipulate',
   git: 'https://github.com/jeanfredrik/meteor-reactive-sort-specifier',
   documentation: 'README.md'
